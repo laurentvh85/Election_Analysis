c32d76d537d65b9ee3769a3881be6d10ad1fd55e
Laurent Van Hassel <laurent.vanhassel@me.com>
2020-06-05 16:52:27 -0400

    out for ice cream

--- python_practice.py
+++ python_practice.py
@@ -6,12 +6,34 @@ file_to_load = os.path.join("Resources/election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#total vote count
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-    # To do: read and analyze the data here.
-       # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
-     # Print the header row.
+
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        total_votes += 1
+        print(total_votes)
+
+        #print candidate name
+        candidate_name = row [2]
+        if candidate_name not in candidate_options:
+            #add candidate name
+            candidate_options.append(candidate_name)
+
+            #track candidate vote
+            candidate_votes[candidate_name] = 0
+
+
+#print candidate list
+print(candidate_votes)
+
+
